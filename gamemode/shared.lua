f63d9c8de645858c33b43bf3ac9b742e4f853f8f
RedDeadlyCreeper <42948693+RedDeadlyCreeper@users.noreply.github.com>
2020-11-20 22:29:44 -0500

    IDK how g-force acts on the server

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -1544,16 +1544,19 @@ function calculateForceLimits(id,Deltime) --Will boot a player out of their seat
 	--19291*4
 	--7716400 old
 	--129921 ~= 50G
-	if Accel:Length() > (24000) then--129921, 999999999999999  --Pulling about 50g. If you pull this hard you should get something checked out.
-		print(Accel:Length())
+--	if Accel:Length() > (24000) then--129921, 999999999999999  --Pulling about 50g. If you pull this hard you should get something checked out.
+if Accel:Length() > (9999999999) then--129921, 999999999999999  --Pulling about 50g. If you pull this hard you should get something checked out.
+--print(Accel:Length())
 		if IsValid(Driver) then
 		Driver:ExitVehicle()
 --		Driver:Kill()
 		chatMessagePly(Driver, "[TPG] Exceeded G-Limit." , Color( 255, 0, 0 ) )
 		end
 
-	elseif lastVel:Length() > 3500 then --9999999999 , 3500 about 200 mph
-		print(lastVel:Length())
+--	elseif lastVel:Length() > 3500 then --9999999999 , 3500 about 200 mph
+	elseif lastVel:Length() > 9999999999 then --9999999999 , 3500 about 200 mph
+
+---		print(lastVel:Length())
 		if IsValid(Driver) then
 		Driver:ExitVehicle()
 --		Driver:Kill()
